f37246e1e1649a99e2b4530e8de5f882019a6519
qinjiabo1990 <qinjiabo1990@gmail.com>
2021-05-27 08:31:52 +1000

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Body from './App/Body'
+import Footer from './App/Footer'
 
 function App() {
   //passing DATA
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Body />
+      <Footer />
     </>
   );
 }
---
+++ src/App/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <div>
+                <p>© 2021 Weather App. All Rights Reserved. Designed by Bob</p>
+            </div>
+        </>
+    )
+
+};
+
+export default Footer;
